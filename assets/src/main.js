b89aba98c0dbf49d25e00cc76faa9dc620ac9858
Fabio Guarrasi <fabioguarrasi@gmail.com>
2018-04-24 00:07:26 +0100

    adding requirejs json plugin

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,15 +1,16 @@
 require.config({
   paths: {
-    'ES6Promise': 'https://cdnjs.cloudflare.com/ajax/libs/es6-promise/4.1.1/es6-promise.auto.min',
     'text': 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
+    'json': 'https://cdnjs.cloudflare.com/ajax/libs/requirejs-plugins/1.0.3/json.min',
     'vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.1/vue.min',
     'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/2.4.0/vuex.min',
+    'ES6Promise': 'https://cdnjs.cloudflare.com/ajax/libs/es6-promise/4.1.1/es6-promise.auto.min',
     'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min'
   },
   shim: {
-    ES6Promise: { exports: 'ES6Promise' },
     vue: { exports: 'Vue' },
     vuex: { exports: 'Vuex' },
+    ES6Promise: { exports: 'ES6Promise' },
     underscore: { exports: 'underscore' }
   },
   baseUrl: 'src'
